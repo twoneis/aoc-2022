3a495baa3998df4add369afb488a9a3c2ed46d2e
Sanjay Chacku Purakal <sanjay29@sapura.de>
2022-12-01 11:54:39 +0100

    init

---
+++ day1/target/debug/deps/day1-4398c410b3cf7d0f.d
@@ -0,0 +1,5 @@
+/home/sanjay/aoc2022/day1/target/debug/deps/day1-4398c410b3cf7d0f.rmeta: src/main.rs
+
+/home/sanjay/aoc2022/day1/target/debug/deps/day1-4398c410b3cf7d0f.d: src/main.rs
+
+src/main.rs:
---
+++ day1/target/debug/deps/day1-63ccfe024879e78d.d
@@ -0,0 +1,5 @@
+/home/sanjay/aoc2022/day1/target/debug/deps/day1-63ccfe024879e78d.rmeta: src/main.rs
+
+/home/sanjay/aoc2022/day1/target/debug/deps/day1-63ccfe024879e78d.d: src/main.rs
+
+src/main.rs:
