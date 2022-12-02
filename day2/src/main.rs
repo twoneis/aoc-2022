a8db477043e62d7b7eb129deeb60a0bdede247fc
Sanjay Chacku Purakal <sanjay29@sapura.de>
2022-12-02 07:08:35 +0100

    finished

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -10,12 +10,12 @@ fn main() {
     for line in split_inputs {
         let mut split = line.split_whitespace();
 
-        let mut player_one_move =
+        let player_one_move =
             part_one_player_one(split.next().expect("Error in splitting line; first part"));
-        let mut player_two_move_part_one =
+        let player_two_move_part_one =
             part_one_player_two(split.next().expect("Error in splitting line; second part"));
 
-        let mut player_two_move_part_two =
+        let player_two_move_part_two =
             part_two_player_two(player_one_move, player_two_move_part_one);
 
         player_two_score_part_one += player_two_move_part_one;
@@ -27,11 +27,25 @@ fn main() {
         } else if player_one_move == player_two_move_part_one {
             player_two_score_part_one += 3;
         }
+
+        player_two_score_part_two += player_two_move_part_two;
+
+        if player_two_move_part_two == player_one_move + 1
+            || (player_one_move == 3 && player_two_move_part_two == 1)
+        {
+            player_two_score_part_two += 6;
+        } else if player_one_move == player_two_move_part_two {
+            player_two_score_part_two += 3;
+        }
     }
 
     println!(
         "Player two score for part one is {}",
         player_two_score_part_one
+    );
+    println!(
+        "Player two score for part two is {}",
+        player_two_score_part_two
     );
 }
 
@@ -53,6 +67,20 @@ fn part_one_player_two(player_move: &str) -> i32 {
     }
 }
 
-fn part_two_player_two(player_one_move: i32, result: i32) {
-    if result = 
+fn part_two_player_two(player_one_move: i32, result: i32) -> i32 {
+    let mut ret_val = 0;
+    if result == 2 {
+        ret_val = player_one_move;
+    } else if result == 1 {
+        ret_val = player_one_move - 1;
+        if ret_val == 0 {
+            ret_val = 3
+        }
+    } else if result == 3 {
+        ret_val = player_one_move + 1;
+        if ret_val == 4 {
+            ret_val = 1
+        }
+    }
+    return ret_val;
 }
