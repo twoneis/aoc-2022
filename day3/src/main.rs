211693ba5a6d253edd3a546d1a95b82da6cd215d
Sanjay Chacku Purakal <sanjay29@sapura.de>
2022-12-03 07:04:19 +0100

    part one done

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,23 +1,35 @@
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("test_inputs.txt").expect("Error reading input file");
+    let input = fs::read_to_string("inputs.txt").expect("Error reading input file");
     let split_inputs = input.lines();
+    let mut priority_sum = 0;
 
     for line in split_inputs {
         let items = line.split_at(line.len()/2);
         let duplicate_char = get_dup_letter(items);
-        println!("{}", duplicate_char.to_digit(10).expect("not valid char"));
+        let priority = letter_to_u32(duplicate_char);
+        priority_sum += priority;
     }
+
+    println!("{}", priority_sum);
 }
 
 fn get_dup_letter (items: (&str, &str)) -> char{
     for first_word_letter in items.0.chars() {
-        for second_word_letter in items.0.chars() {
+        for second_word_letter in items.1.chars() {
             if first_word_letter.eq(&second_word_letter) {
                 return first_word_letter;
             }
         }
     }
     return 'Z';
+}
+
+fn letter_to_u32 (letter: char) -> u32 {
+    let mut ret_val = letter.to_digit(36).expect("not valid letter") - 9;
+    if letter.is_uppercase() {
+        ret_val += 26;
+    }
+    return ret_val;
 }
