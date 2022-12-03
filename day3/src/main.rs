058c56528d7f585846307637055c9706d8ac600e
Sanjay Chacku Purakal <sanjay29@sapura.de>
2022-12-03 23:56:17 +0100

    finished day 3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,15 +4,24 @@ fn main() {
     let input = fs::read_to_string("inputs.txt").expect("Error reading input file");
     let split_inputs = input.lines();
     let mut priority_sum = 0;
+    let mut group: Vec<&str> = Vec::new();
+    let mut group_sum = 0;
 
     for line in split_inputs {
         let items = line.split_at(line.len()/2);
         let duplicate_char = get_dup_letter(items);
         let priority = letter_to_u32(duplicate_char);
         priority_sum += priority;
+
+        group.push(line);
+        if group.len() == 3 {
+            group_sum += letter_to_u32(get_group_letter(&group));
+            group.clear();
+        }
     }
 
     println!("{}", priority_sum);
+    println!("{}", group_sum);
 }
 
 fn get_dup_letter (items: (&str, &str)) -> char{
@@ -20,6 +29,21 @@ fn get_dup_letter (items: (&str, &str)) -> char{
         for second_word_letter in items.1.chars() {
             if first_word_letter.eq(&second_word_letter) {
                 return first_word_letter;
+            }
+        }
+    }
+    return 'Z';
+}
+
+fn get_group_letter (group: &Vec<&str>) -> char {
+    for first_bag_letter in group[0].chars() {
+        for second_bag_letter in group[1].chars() {
+            if first_bag_letter.eq(&second_bag_letter) {
+                for third_bag_letter in group[2].chars() {
+                    if first_bag_letter.eq(&third_bag_letter) {
+                        return first_bag_letter;
+                    }
+                }
             }
         }
     }
