3d5b66551981752c1a32cf39c8b92f5b65a6f6f4
Sanjay Chacku Purakal <sanjay29@sapura.de>
2022-12-06 10:49:50 +0100

    part one done

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,5 +1,34 @@
-use std::fs;
+use std::{collections::VecDeque, fs};
 
 fn main() {
     let input = fs::read_to_string("inputs.txt").expect("error reading file");
+
+    let mut recent_four_chars: VecDeque<char> = VecDeque::new();
+    let mut buffer_counter = 0;
+    let mut no_dups = false;
+
+    for received_char in input.chars() {
+        recent_four_chars.push_back(received_char);
+        buffer_counter += 1;
+        if recent_four_chars.len() > 4 {
+            recent_four_chars.pop_front();
+            let mut i = 0;
+            no_dups = true;
+            while i < recent_four_chars.len() {
+                let mut j = i + 1;
+                while j < recent_four_chars.len() {
+                    if recent_four_chars[i] == recent_four_chars[j] {
+                        no_dups = false;
+                    }
+                    j += 1;
+                }
+                i += 1;
+            }
+            if no_dups {
+                break;
+            }
+        }
+    }
+
+    println!("{buffer_counter}");
 }
