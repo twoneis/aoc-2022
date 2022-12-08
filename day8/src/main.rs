922fdde7f7b3428006b29243a6da699e8e0b959b
Sanjay Chacku Purakal <sanjay29@sapura.de>
2022-12-08 06:46:53 +0100

    tree visibility working (?)

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("inputs.txt").expect("error reading file");
+    let input = fs::read_to_string("test.txt").expect("error reading file");
     let lines = input.lines();
 
     let mut line_count = 0;
@@ -23,10 +23,63 @@ fn main() {
         row_num += 1;
     }
 
-    for tree_row in tree_grid {
+    let mut row_num = 0;
+    let mut column_num = 0;
+    for tree_row in tree_grid.clone() {
+        column_num = 0;
         for tree in tree_row {
-            print!("{} ", tree);
+            if test_tree_visibility(row_num, column_num, tree_grid.clone()) {
+                print!("{} ", tree);
+            } else {
+                print!("  ");
+            }
+            column_num += 1;
         }
+        row_num += 1;
         print!("\n")
     }
+}
+
+fn test_tree_visibility(x: usize, y: usize, tree_grid: Vec<Vec<i32>>) -> bool {
+    let tree_to_test = tree_grid[x][y];
+
+    let mut tree_visible_left = true;
+    let mut tree_visible_right = true;
+    let mut tree_visible_front = true;
+    let mut tree_visible_back = true;
+
+    let mut row_num = 0;
+    let mut column_num = 0;
+    for tree_row in tree_grid {
+        column_num = 0;
+        for tree in tree_row {
+            if tree >= tree_to_test && (column_num == y || row_num == x) {
+                //println!("comparing {} against {}", tree, tree_to_test);
+
+                if column_num == y && row_num < x {
+                    tree_visible_left = false;
+                    //println!("not visible from left");
+                }
+                if column_num == y && row_num > x {
+                    tree_visible_right = false;
+                    //println!("not visible from right");
+                }
+                if column_num < y && row_num == x {
+                    tree_visible_back = false;
+                    //println!("not visible from back");
+                }
+                if column_num > y && row_num == x {
+                    tree_visible_front = false;
+                    //println!("not visible from front");
+                }
+            }
+            column_num += 1;
+        }
+        row_num += 1;
+    }
+
+    if tree_visible_left || tree_visible_right || tree_visible_front || tree_visible_back {
+        return true;
+    }
+    return false;
 }
