e974a123d75ef49cabce39c02b39dd665a7a7ced
Sanjay Chacku Purakal <sanjay29@sapura.de>
2022-12-08 06:19:16 +0100

    getting input as vec grid

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("inputs.txt").expect("error reading file");
+    let lines = input.lines();
+
+    let mut line_count = 0;
+    for line in input.lines() {
+        line_count += 1;
+    }
+
+    let mut tree_grid = vec![vec![0; line_count]; line_count];
+
+    let mut row_num = 0;
+    let mut column_num = 0;
+    for line in lines {
+        column_num = 0;
+        for tree in line.chars() {
+            let tree_val = tree as i32 - '0' as i32;
+            tree_grid[row_num][column_num] = tree_val;
+            column_num += 1;
+        }
+        row_num += 1;
+    }
+
+    for tree_row in tree_grid {
+        for tree in tree_row {
+            print!("{} ", tree);
+        }
+        print!("\n")
+    }
 }
